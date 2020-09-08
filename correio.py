ceb88c580c0e668ab262f8c60598f24972edbc55
RMeirelles <ricardinho951@hotmail.com>
2020-09-08 20:33:42 -0300

    bs4 implementado

--- correio.py
+++ correio.py
@@ -2,22 +2,21 @@ import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-cep = str(14051120)
+cep = str(14030000)
 
 payload = {'relaxation': cep, 'semelhante': 'N', 'tipoCEP': 'ALL'}
 r = requests.post("http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm", data=payload)
 
 conteudo = BeautifulSoup(r.content)
 
-
-
-#conteudo = str((r.content))
-
-#cep = cep[:5]+'-'+cep[-3:]
-#inicio = conteudo.index('Logradouro/Nome:')
-
-#fim = conteudo.index(cep)+9
-
-#print(conteudo[inicio:fim])
-
 print(conteudo.prettify())
+
+# conteudo = str((r.content))
+
+# cep = cep[:5]+'-'+cep[-3:]
+# inicio = conteudo.index('Logradouro/Nome:')
+
+# fim = conteudo.index(cep)+9
+
+# print(conteudo[inicio:fim])
+
