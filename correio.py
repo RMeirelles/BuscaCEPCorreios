f75c0e82095455caee26166d9635441c07a8d2b6
RMeirelles <ricardinho951@hotmail.com>
2020-09-09 00:22:32 -0300

    Inclusão das validações

--- correio.py
+++ correio.py
@@ -1,27 +1,41 @@
 import requests
 import json
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-cep = str(14020570)
 
-payload = {'relaxation': cep, 'semelhante': 'N', 'tipoCEP': 'ALL'}
-xml_data = requests.post("http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm", data=payload).content
+print('Qual o cep a ser pesquisado?')
 
-if __name__ == '__main__':
-    model = BeautifulSoup(xml_data, features='lxml')
-    fields = []
-    table_data = []
-    for tr in model.table.find_all('tr', recursive=False):
-        for th in tr.find_all('th', recursive=False):
-            fields.append(th.text)
-    for tr in model.table.find_all('tr', recursive=False):
-        datum = {}
-        for i, td in enumerate(tr.find_all('td', recursive=False)):
-            datum[fields[i]] = td.text
-        if datum:
-            table_data.append(datum)
+while True:
 
-    print(json.dumps(table_data, indent=4))
+    cep = str(input('> '))
 
-#print(conteudo.findAll("table", class_="tmptabela"))
+    if len(cep) == 8 and cep.isnumeric() :
+            
+        payload = {'relaxation': cep, 'semelhante': 'N', 'tipoCEP': 'ALL'}
+        html_data = requests.post("http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm", data=payload).text
+
+        if BeautifulSoup(html_data, features='lxml').table is None:
+            print('O CEP não foi encontrado na base de dados!')
+
+        else:
+
+            if __name__ == '__main__':
+                model = BeautifulSoup(html_data, features='lxml')
+                tituloCampo = []
+                dataCampo = []
+                for tr in model.table.find_all('tr', recursive=False):
+                    for th in tr.find_all('th', recursive=False):
+                        tituloCampo.append(th.text)
+                for tr in model.table.find_all('tr', recursive=False):
+                    datum = {}
+                    for i, td in enumerate(tr.find_all('td', recursive=False)):
+                        datum[tituloCampo[i]] = td.text
+                    if datum:
+                        dataCampo.append(datum)
+
+
+            print(json.dumps(dataCampo, indent=2, ensure_ascii=False))
+            break
+
+    else:
+        print('Digite um cep válido!')
