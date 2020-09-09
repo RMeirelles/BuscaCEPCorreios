f3d44c9776695e40ec18ea2b9900a8eca40eeede
RMeirelles <ricardinho951@hotmail.com>
2020-09-08 21:43:39 -0300

    implementacao do json

--- correio.py
+++ correio.py
@@ -1,22 +1,27 @@
 import requests
+import json
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-cep = str(14030000)
+cep = str(14020570)
 
 payload = {'relaxation': cep, 'semelhante': 'N', 'tipoCEP': 'ALL'}
-r = requests.post("http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm", data=payload)
+xml_data = requests.post("http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm", data=payload).content
 
-conteudo = BeautifulSoup(r.content)
+if __name__ == '__main__':
+    model = BeautifulSoup(xml_data, features='lxml')
+    fields = []
+    table_data = []
+    for tr in model.table.find_all('tr', recursive=False):
+        for th in tr.find_all('th', recursive=False):
+            fields.append(th.text)
+    for tr in model.table.find_all('tr', recursive=False):
+        datum = {}
+        for i, td in enumerate(tr.find_all('td', recursive=False)):
+            datum[fields[i]] = td.text
+        if datum:
+            table_data.append(datum)
 
-print(conteudo.prettify())
+    print(json.dumps(table_data, indent=4))
 
-# conteudo = str((r.content))
-
-# cep = cep[:5]+'-'+cep[-3:]
-# inicio = conteudo.index('Logradouro/Nome:')
-
-# fim = conteudo.index(cep)+9
-
-# print(conteudo[inicio:fim])
-
+#print(conteudo.findAll("table", class_="tmptabela"))
